ddae9a2aac1797795f9e958bef23512005e475a1
Kevin Dang <dangkevin12@gmail.com>
2026-02-08 13:57:23 -0500

    add named components

---
+++ src/app/error.tsx
@@ -0,0 +1,9 @@
+export default function RouteError() {
+  return (
+    <main className="mx-auto flex w-full max-w-xl flex-col gap-4 p-6">
+      <h1 className="text-2xl font-semibold tracking-tight">
+        Something went wrong
+      </h1>
+    </main>
+  );
+}
