6299c5f7dcf172611d7bf02ea4e73636de84714a
Kevin Dang <dangkevin12@gmail.com>
2026-02-17 23:22:14 -0500

    clean up transactions page

--- src/app/transactions/_components/add-transaction-dialog-loader.tsx
+++
@@ -1,7 +0,0 @@
-import { AddTransactionDialog } from "@/components/transactions/add-transaction-dialog";
-import { getDistinctCategories } from "@/db/queries/transactions";
-
-export async function AddTransactionDialogLoader() {
-  const categories = await getDistinctCategories();
-  return <AddTransactionDialog categories={categories} />;
-}
