c848e1ff2330979c07be2b9ef77903125d1e379e
Kevin Dang <dangkevin12@gmail.com>
2026-02-20 00:49:29 -0500

    fix suspense

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,14 @@
 import { SignIn } from "@clerk/nextjs";
+import { connection } from "next/server";
+import { Suspense } from "react";
 
-export default function SignInPage() {
+export default async function SignInPage() {
+  await connection();
   return (
     <div className="flex min-h-[calc(100vh-3.5rem)] items-center justify-center">
-      <SignIn />
+      <Suspense>
+        <SignIn />
+      </Suspense>
     </div>
   );
 }
